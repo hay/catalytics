258543a1576924938f7b40ee4edea6b7045ff80a
Hay Kranen <huskyr@gmail.com>
2019-04-13 00:13:29 +0200

    Commiting v1.2

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-[].slice.call(document.querySelectorAll('.ga-hero img')).forEach(function(img) {
-    var file = img.src.split('/').pop();
-    var url = chrome.extension.getURL("img/" + file);
-    img.src = url;
+const $ = document.querySelector.bind(document);
+
+function cat() {
+    var nr = Math.round(Math.random() * 2) + 1;
+    return chrome.extension.getURL("img/cat" + nr + ".jpg");
+}
+
+function $$(selector) {
+    return Array.from(document.querySelectorAll(selector));
+}
+
+const IMAGES = [
+    '.blockquote-photo',
+    '.img-incol',
+    '.gmp-banner img',
+    '.gmp-no-grab',
+    'img'
+];
+
+const SRCSET = [
+    '.blockquote-photo',
+    '.img-incol',
+    '.gmp-banner source',
+    'source'
+];
+
+const BGIMAGES = [
+    '.hero-image'
+];
+
+IMAGES.forEach((selector) => {
+    $$(selector).forEach(i => i.src = cat());
 });
+
+SRCSET.forEach((selector) => {
+    $$(selector).forEach(i => i.srcset = cat());
+});
+
+BGIMAGES.forEach((selector) => {
+    $$(selector).forEach(i => i.style.backgroundImage = `url(${cat()})`)
+});
+
+$$("h1, h2, h3").forEach(h1 => h1.innerHTML = 'miaow.');
+
+$(".photo-tout__background").style.opacity = 0;
